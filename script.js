794cccceae7601ac0927ed09fc65c78c767a44f5
giorgiochirinos <57816350+giorgiochirinos@users.noreply.github.com>
2024-12-02 21:46:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-import name from "resource.js";
+import name from "./resource.js";
 //import { name, draw, reportArea, reportPerimeter } from "./modules/square.js";
 console.log(`value imported: ${name}`);
